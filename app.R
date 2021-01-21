deb1496f7f8eb94c54d72e748af8efde64d8f4bd
chrl3hr5 <chrl3hr5@gmail.com>
2021-01-21 16:32:26 +0530

    Marine Traffic web application using R Shiny

---
+++ app.R
@@ -0,0 +1,26 @@
+# Loading libraries
+library(shiny) # For creating R Shiny web app
+library(shiny.semantic) # For advance UI options
+library(tidyverse) # For data manipulation and analysis
+
+# Sourcing external R scripts
+source("Data.R") # For loading data
+source("Type.R") # For selecting vessel type
+source("Name.R") # For selecting vessel name
+
+# User Interface (UI)
+ui <- semanticPage(
+  TypeUI(Data),
+  NameUI(Data)
+)
+
+# Server
+server <- function(session, input, output) {
+  observe({
+    Temp <- as.vector(as.matrix(unique(Data %>% filter(ship_type == input$Type) %>% select(SHIPNAME))))
+    update_dropdown_input(session, input_id = "Name", choices = Temp)
+  })
+}
+
+# Application
+shinyApp(ui = ui, server = server)
