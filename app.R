3991ef4158b6429215ba9df2b4f8a39f8c4fa8c8
chrl3hr5 <chrl3hr5@gmail.com>
2021-01-23 01:32:20 +0530

    Successful execution of module

--- app.R
+++ app.R
@@ -4,25 +4,17 @@ library(shiny.semantic) # For advance UI options
 library(tidyverse) # For data manipulation and analysis
 
 # Sourcing external R scripts
-source("Data.R") # For loading data
-source("Type.R") # For selecting vessel type
-source("Name.R") # For selecting vessel name
+source("data.R") # For loading data
+source("vessel.R") # For selecting vessel type and name
 
 # User Interface (UI)
 ui <- semanticPage(
-  TypeUI("type", Data),
-  NameUI("name", Data)
+  VesselUI("vessel", Data)
 )
 
 # Server
 server <- function(session, input, output) {
-  Value <- reactiveValues(Type = NULL)
-  Temp <- callModule(TypeServer, "type", Data)
-  observe({
-    Value$Type <- Temp$Type
-    Temp <- as.vector(as.matrix(unique(Data %>% filter(ship_type == Value$Type) %>% select(SHIPNAME))))
-    update_dropdown_input(session, input_id = "Name", choices = Temp)
-  })
+  callModule(VesselServer, "vessel", Data)
 }
 
 # Application
