539042baeb108507cd87f7621ee4bb360c11c0c1
chrl3hr5 <chrl3hr5@gmail.com>
2021-01-23 08:44:07 +0530

    Minor text editing

--- app.R
+++ app.R
@@ -2,26 +2,26 @@
 library(shiny) # For creating R Shiny web app
 library(shiny.semantic) # For advance UI options
 library(tidyverse) # For data manipulation and analysis
-library(geosphere) # For calculating distance between coordindates
+library(geosphere) # For calculating distance between coordinates
 
 # Sourcing external R scripts
 source("load_data.R") # For loading data
 source("vessel.R") # For executing shiny module to select vessel type and name
-source("distance.R") # For calculating the distance traveled by a vessel (in m)
+source("distance.R") # For calculating the distance traveled by a vessel
 
 # User Interface (UI)
 ui <- semanticPage(
-  # Executing UI from vessel module
+  # Executing UI from "Vessel" module
   VesselUI("vessel", Data),
-  # Executing UI from distance module
+  # Executing UI from "Distance" module
   DistanceUI("distance")
 )
 
 # Server
 server <- function(session, input, output) {
-  # Calling server from vessel module
+  # Calling server from "Vessel" module
   `Vessel Data` <- callModule(VesselServer, "vessel", Data)
-  # Calculating the distance traveled by the selected vessel
+  # Calling server from "Distance" module
   callModule(DistanceServer, "distance", Data, `Vessel Data`)
 }
 
