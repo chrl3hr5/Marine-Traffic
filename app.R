692647d6014e6363cb1c60a762dec1cec5ef61a4
chrl3hr5 <chrl3hr5@gmail.com>
2021-01-23 08:36:54 +0530

    Complete code for vessel selection and total distance measurement

--- app.R
+++ app.R
@@ -2,19 +2,27 @@
 library(shiny) # For creating R Shiny web app
 library(shiny.semantic) # For advance UI options
 library(tidyverse) # For data manipulation and analysis
+library(geosphere) # For calculating distance between coordindates
 
 # Sourcing external R scripts
 source("load_data.R") # For loading data
-source("vessel.R") # For selecting vessel type and name
+source("vessel.R") # For executing shiny module to select vessel type and name
+source("distance.R") # For calculating the distance traveled by a vessel (in m)
 
 # User Interface (UI)
 ui <- semanticPage(
-  VesselUI("vessel", Data)
+  # Executing UI from vessel module
+  VesselUI("vessel", Data),
+  # Executing UI from distance module
+  DistanceUI("distance")
 )
 
 # Server
 server <- function(session, input, output) {
-  callModule(VesselServer, "vessel", Data)
+  # Calling server from vessel module
+  `Vessel Data` <- callModule(VesselServer, "vessel", Data)
+  # Calculating the distance traveled by the selected vessel
+  callModule(DistanceServer, "distance", Data, `Vessel Data`)
 }
 
 # Application
