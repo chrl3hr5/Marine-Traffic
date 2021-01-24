8508f9d74a6d981cfb173ea9221afd686ffbc37c
chrl3hr5 <chrl3hr5@gmail.com>
2021-01-24 21:22:32 +0530

    Enclosing text output in a box

--- modules/longest_distance.R
+++ modules/longest_distance.R
@@ -1,13 +1,16 @@
 # Distance module's User Interface (UI)
-DistanceUI <- function(id) {
-  tagList(
-    textOutput(outputId = NS(id, "Distance")),
-    textOutput(outputId = NS(id, "Observation"))
+Longest_DistanceUI <- function(id) {
+  fluidRow(
+    box(
+      width = 12, ribbon = T, title = "NOTE",
+      title_side = "top right", collapsible = F,
+      textOutput(outputId = NS(id, "Observation"))
+    )
   )
 }
 
 # Distance module's Server
-DistanceServer <- function(input, output, session, data, info) {
+Longest_DistanceServer <- function(input, output, session, data, info) {
   observe({
     # Filtering data on the basis of vessel type and name
     Temp <- data %>% filter(ship_type == info$Type() & SHIPNAME == info$Name())
@@ -18,15 +21,18 @@ DistanceServer <- function(input, output, session, data, info) {
     Travel <- apply(as.data.frame(cbind(First, Second)), 1, function(x) {
       distGeo(c(x[1], x[2]), c(x[3], x[4]))
     })
-    # Total distance traveled (in meters)
-    output$Distance <- renderText({
-      paste0("Total distance traveled by the vessel is ", round(sum(Travel)), " (meters).")
-    })
     # Finding longest distance traveled between consecutive observations
     Index <- which(Travel == max(Travel))[length(which(Travel == max(Travel)))]
     # Finding the observations associated with longest distance
     output$Observation <- renderText({
-      paste0("The longest distance of ", round(Travel[Index]), " (meters) was traveled between ", Temp$DATETIME[Index], " and ", Temp$DATETIME[Index + 1], " by the vessel.")
+      paste0(
+        "The map shows the selected vessel's initial 
+             and final positions for those two consecutive 
+             observations which have the most separation of ",
+        round(Travel[Index]), " (meters) and was traveled between ",
+        Temp$DATETIME[Index], " and ", Temp$DATETIME[Index + 1],
+        " by the vessel."
+      )
     })
   })
 }
