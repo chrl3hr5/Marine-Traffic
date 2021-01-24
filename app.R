b91472243ced23dc68589954e12a5b3569a7658e
chrl3hr5 <chrl3hr5@gmail.com>
2021-01-24 17:48:46 +0530

    Added libraries and made changes in module execution

--- app.R
+++ app.R
@@ -1,20 +1,28 @@
 # Loading libraries
+library(data.table) # For reading data
+library(feather) # For reading and storing data
 library(shiny) # For creating R Shiny web app
 library(shiny.semantic) # For advance UI options
 library(tidyverse) # For data manipulation and analysis
-library(geosphere) # For calculating distance between coordinates
+library(geosphere) # For calculating distance between coordinates (in meters)
+library(leaflet) # For creating maps
+library(maptools) # For shape files
 
 # Sourcing external R scripts
 source("external/load_data.R") # For loading data
-source("modules/vessel.R") # For executing shiny module to select vessel type and name
-source("modules/distance.R") # For calculating the distance traveled by a vessel
+source("external/load_polygons.R") # For loading world polygons
+source("modules/vessel.R") # For selecting vessel type and name
+source("modules/distance.R") # For calculating the distance traveled by a vessel (in meters)
+source("modules/map.R") # For creating a map to display vessel positions
 
 # User Interface (UI)
 ui <- semanticPage(
   # Executing UI from "Vessel" module
   VesselUI("vessel", Data),
   # Executing UI from "Distance" module
-  DistanceUI("distance")
+  DistanceUI("distance"),
+  # Executing UI from "Map" module
+  MapUI("map")
 )
 
 # Server
@@ -23,6 +31,8 @@ server <- function(session, input, output) {
   `Vessel Data` <- callModule(VesselServer, "vessel", Data)
   # Calling server from "Distance" module
   callModule(DistanceServer, "distance", Data, `Vessel Data`)
+  # Calling server from "Map" module
+  callModule(MapServer, "map", Data, `Vessel Data`, wrld_simpl)
 }
 
 # Application
