9c10096a302efd0fe06d2323fe9dcb5108f91496
chrl3hr5 <chrl3hr5@gmail.com>
2021-01-24 21:20:22 +0530

    Adding space between boxes

--- modules/vessel.R
+++ modules/vessel.R
@@ -1,10 +1,16 @@
 # Vessel module's User Interface (UI)
 VesselUI <- function(id, data) {
-  tagList(
-    # Dropdown menu for selecting vessel type
-    dropdown_input(input_id = NS(id, "Type"), choices = unique(data$ship_type), value = unique(data$ship_type)[1], type = "selection fluid"),
-    # Dropdown menu for selecting vessel name
-    dropdown_input(input_id = NS(id, "Name"), choices = NULL, type = "selection fluid")
+  fluidRow(
+    box(
+      width = 12, title = "SELECT VESSEL TYPE", collapsible = F,
+      # Dropdown menu for selecting vessel type
+      dropdown_input(input_id = NS(id, "Type"), choices = unique(data$ship_type), value = unique(data$ship_type)[1], type = "selection fluid")
+    ), br(),
+    box(
+      width = 12, title = "SELECT VESSEL NAME", collapsible = F,
+      # Dropdown menu for selecting vessel name
+      dropdown_input(input_id = NS(id, "Name"), choices = NULL, type = "selection fluid")
+    )
   )
 }
 
