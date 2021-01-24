0b67afaa37ad44d9c976c56e0b3665cef98e210c
chrl3hr5 <chrl3hr5@gmail.com>
2021-01-24 21:20:38 +0530

    Adding value boxes

--- modules/total_distance.R
+++ modules/total_distance.R
@@ -1,8 +1,16 @@
 # Distance module's User Interface (UI)
 Total_DistanceUI <- function(id) {
-  tagList(
-    textOutput(outputId = NS(id, "Distance")),
-    textOutput(outputId = NS(id, "Observation"))
+  fluidRow(
+    split_layout(
+      # Adding value box for vessel's length
+      value_box_output(outputId = NS(id, "Length")),
+      # Adding value box for vessel's width
+      value_box_output(outputId = NS(id, "Width")),
+      # Adding value box for vessel's deadweight
+      value_box_output(outputId = NS(id, "Deadweight")),
+      # Adding value box for the total distance covered by the vessel
+      value_box_output(outputId = NS(id, "Distance"))
+    )
   )
 }
 
@@ -18,15 +26,29 @@ Total_DistanceServer <- function(input, output, session, data, info) {
     Travel <- apply(as.data.frame(cbind(First, Second)), 1, function(x) {
       distGeo(c(x[1], x[2]), c(x[3], x[4]))
     })
-    # Total distance traveled (in meters)
-    output$Distance <- renderText({
-      paste0("Total distance traveled by the vessel is ", round(sum(Travel)), " (meters).")
+    # Total length of the vessel (in meters)
+    output$Length <- render_value_box({
+      valueBox(
+        value = unique(Temp$LENGTH), subtitle = "Length of the vessel (in meters)"
+      )
     })
-    # Finding longest distance traveled between consecutive observations
-    Index <- which(Travel == max(Travel))[length(which(Travel == max(Travel)))]
-    # Finding the observations associated with longest distance
-    output$Observation <- renderText({
-      paste0("The longest distance of ", round(Travel[Index]), " (meters) was traveled between ", Temp$DATETIME[Index], " and ", Temp$DATETIME[Index + 1], " by the vessel.")
+    # Total width of the vessel (in meters)
+    output$Width <- render_value_box({
+      valueBox(
+        value = unique(Temp$WIDTH), subtitle = "Width of the vessel (in meters)"
+      )
+    })
+    # Total deadweight of the vessel (in tonnes)
+    output$Deadweight <- render_value_box({
+      valueBox(
+        value = unique(Temp$DWT), subtitle = "Deadweight of the vessel (in tonnes)"
+      )
+    })
+    # Total distance traveled (in meters)
+    output$Distance <- render_value_box({
+      valueBox(
+        value = round(sum(Travel)), subtitle = "Total distance traveled (in meters)"
+      )
     })
   })
 }
