3372326803b823feccd36b6d6a9cb8cfc3c38ccd
chrl3hr5 <chrl3hr5@gmail.com>
2021-01-24 17:46:10 +0530

    Added code for calculating longest distance between consecutive observations

--- modules/distance.R
+++ modules/distance.R
@@ -1,7 +1,8 @@
 # Distance module's User Interface (UI)
 DistanceUI <- function(id) {
   tagList(
-    textOutput(outputId = NS(id, "TextOutput"))
+    textOutput(outputId = NS(id, "Distance")),
+    textOutput(outputId = NS(id, "Observation"))
   )
 }
 
@@ -13,13 +14,19 @@ DistanceServer <- function(input, output, session, data, info) {
     # Segregating data for performing calculations
     First <- Temp[-nrow(Temp), c("LAT", "LON")]
     Second <- Temp[-1, c("LAT", "LON")]
-    # Calculating the separation between each pair of coordinates
+    # Calculating the separation between each pair of coordinates (in meters)
     Travel <- apply(as.data.frame(cbind(First, Second)), 1, function(x) {
-      distCosine(c(x[1], x[2]), c(x[3], x[4]))
+      distGeo(c(x[1], x[2]), c(x[3], x[4]))
     })
-    # Total distance traveled
-    output$TextOutput <- renderText({
-      sum(Travel)
+    # Total distance traveled (in meters)
+    output$Distance <- renderText({
+      paste0("Total distance traveled by the vessel is ", round(sum(Travel)), " (meters).")
+    })
+    # Finding longest distance traveled between consecutive observations
+    Index <- which(Travel == max(Travel))[length(which(Travel == max(Travel)))]
+    # Finding the observations associated with longest distance
+    output$Observation <- renderText({
+      paste0("The longest distance of ", round(Travel[Index]), " (meters) was traveled between ", Temp$DATETIME[Index], " and ", Temp$DATETIME[Index + 1], " by the vessel.")
     })
   })
 }
